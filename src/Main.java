2edf1ce23bf3f62d4bb56d4f85177655b23ca67d
arbuz <borysbaczewski@protonmail.com>
2022-10-17 10:21:14 +0200

    Main menu body created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\tMENU\t");
+        System.out.println("Choose one option 1 - 4");
+        System.out.println("1. Borrow the book\n2. Return the book to library\n3. Check borrowed books\n4. Check book availability");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n Your choice: ");
+        int choice = scanner.nextInt();
+        scanner.close();
+
+        switch(choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
+
     }
 }
